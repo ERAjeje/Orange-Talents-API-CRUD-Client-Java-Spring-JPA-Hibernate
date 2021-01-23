1145d377844ded0061652aa5af7c9930ffe2f9a3
Edson Ajeje <edson.ajeje@terra.com.br>
2021-01-23 16:00:19 -0300

    Final commit to share code

---
+++ src/main/java/br/com/edsonajeje/cadatroAPI/CadatroApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.edsonajeje.cadatroAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadatroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadatroApiApplication.class, args);
+	}
+
+}
